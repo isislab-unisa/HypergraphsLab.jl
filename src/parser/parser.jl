ff691c391bcc866c2be29c7a7e2696b478ef196b
gilbertrec <gilbertrecupito@gmail.com>
2019-09-21 11:09:00 +0200

    added metadata fetching in load_steam_reviews

--- src/parser/parser.jl
+++ src/parser/parser.jl
@@ -6,9 +6,10 @@ hg_save("hgdata/fb/0.circles.hgf", h_fb)
 export_json("hgdata/fb/0.circles.json", h_fb)
 
 h_steam = load_steam_reviews("graphdata/steam/steam.json",Int,"item_id", "reviews" ,"user_id")
+
 hg_save("hgdata/steam/steam.hgf", h_steam)
 export_json("hgdata/fb/steam.json", h_steam)
-
+get_vertex_meta(h_steam,1)
 h_youtube = loadyoutube("graphdata/youtube/com-youtube.top5000.cmty.txt",Int,'\t')
 hg_save("hgdata/steam/steam.hgf", h_steam)
 export_json("hgdata/fb/steam.json", h_steam)
@@ -17,4 +18,4 @@ export_json("hgdata/fb/steam.json", h_steam)
 
 h_gplus=loadgplus("graphdata/gplus/100129275726588145876.circles",Int,'\t')
 hg_save("hgdata/gplus/100129275726588145876.circles.hgf", h_gplus)
-export_json("hgdata/fb/100129275726588145876.circles.json", h_gplus) 
+export_json("hgdata/fb/100129275726588145876.circles.json", h_gplus)
--- src/parser/steam_reviews.jl
+++ src/parser/steam_reviews.jl
@@ -1,50 +1,36 @@
 function load_steam_reviews(fname::AbstractString, T::Type{<:Real}, vertex_name::AbstractString, vertex_set_name::AbstractString, edge_name::AbstractString)
-        h=Hypergraph{T}(0,0)
+        h=Hypergraph{T,AbstractDict,AbstractDict}(0,0)
         weight = convert(T,1)
         vertices_ids_mapping = Dict{AbstractString,Int}()
         new_id=1
         lines=0
-        nodes=0
-        meta_edge_fetch=false
-        meta_vertex_fetch=false
         io = open(fname,"r") #file name
         for line in eachline(io) #for each hyperedge of the file
             lines+=1
+            h_meta=Dict{AbstractString,Any}()
             vertices_dict= Dict{Int64,T}()
             actual_object=JSON.parse(line)
-            actual_object[edge_name]
-
-            if (!meta_edge_fetch) #fetching metadata for hyperedge
-                i=1
-                for key in keys(actual)
-                    set_hyperedge_meta!(h,key,i)
-                    i+=1
-                end
-                meta_edge_fetch=true
+            for key in keys(actual_object)
+                h_meta[key]=actual_object[key]
             end
-
+            i=1
             vertices_ids=actual_object[vertex_set_name]
             for v_id in vertices_ids #for each vertex of the file
-                old_id = v_id[vertex_name]
-
-                if (!meta_vertex_fetch) #fetching metadata for vertices
-                    i=1
-                    for key in keys(v_id)
-
-                        set_vertex_meta!(h,key,i)
-                        i+=1
-                    end
-                    meta_vertex_fetch=true
+                v_meta=Dict{AbstractString,Any}()
+                for key in keys(v_id)
+                    v_meta[key]=v_id[key]
                 end
-
+                old_id = v_id[vertex_name]
                 if !haskey(vertices_ids_mapping,old_id)
                     add_vertex!(h)
+                    set_vertex_meta!(h,v_meta,new_id)
                     vertices_ids_mapping[old_id] = new_id
                     new_id+=1
                 end
                 vertices_dict[vertices_ids_mapping[old_id]] = weight
             end
             add_hyperedge!(h,vertices = vertices_dict)
+            set_hyperedge_meta!(h,h_meta,lines)
         end
         h
 end
